3e3c168691acd9b898b99675921afa76e4d55995
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-09-21 16:19:32 +0900

    Create 3. 풍선 터뜨리기.cpp

---
+++ programmers/월간 코드 챌린지 시즌1 (9월)/3. 풍선 터뜨리기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr1[1000001],arr2[1000001];
+int solution(vector<int> a) {
+    int answer = a.size();
+    arr1[0] = a[0];arr2[a.size()-1]=a[a.size()-1];
+	for (int i=1 ; i<=a.size()-1 ; i++) {
+		if (arr1[i-1]>a[i]) arr1[i]=a[i];
+		else arr1[i] = arr1[i-1];}
+	for (int i=a.size()-2 ; i>=0 ; i--) {
+		if (arr2[i+1] > a[i]) arr2[i]=a[i];
+		else arr2[i] = arr2[i+1];}
+
+    if(a.size()<3) return answer;
+    else{
+        for(int i=1 ; i<=a.size()-2 ; i++) 
+            if(a[i]>arr1[i] && a[i]>arr2[i]) answer--;
+    } return answer;
+}
