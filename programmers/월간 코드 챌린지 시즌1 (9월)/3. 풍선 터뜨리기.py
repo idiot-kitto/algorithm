1f292bf9f5bbd61e8827d90672332453ba862513
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-09-21 16:49:33 +0900

    #prefix, suffix min

--- programmers/월간 코드 챌린지 시즌1 (9월)/3. 풍선 터뜨리기.py
+++ programmers/월간 코드 챌린지 시즌1 (9월)/3. 풍선 터뜨리기.py
@@ -5,13 +5,13 @@ def solution(a):
     arr1[0] = a[0]
     arr2[len(a)-1] = a[len(a)-1]
     for i in range(1,len(a)):
-        if arr1[i-1]>a[i]: arr1[i] = a[i]
+        if arr1[i-1] > a[i]: arr1[i] = a[i]
         else: arr1[i] = arr1[i-1]
     for i in range(len(a)-2,0,-1):
-        if arr2[i+1]>a[i]: arr2[i] = a[i]
+        if arr2[i+1] > a[i]: arr2[i] = a[i]
         else: arr2[i] = arr2[i+1]
     if(len(a)<3): return answer
     else:
         for i in range(1,len(a)-1):
-            if a[i]>arr1[i] and a[i]>arr2[i]: answer -= 1
+            if a[i] > arr1[i] and a[i] > arr2[i]: answer -= 1
     return answer
