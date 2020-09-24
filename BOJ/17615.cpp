fbbfc9f6df794a152f4e4797cf5feab8ebc5cb2f
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-09-24 18:22:15 +0900

    #볼 모으기 #C++ #Greedy #Silver1

--- BOJ/17615.cpp
+++ BOJ/17615.cpp
@@ -13,9 +13,24 @@ const long double PI = 3.141592653589793238462643383279502884197;
 
 ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
 
-int n, R, B, ans = INT_MAX, tmp;
-bool check;
+int n, R, B, ans = INT_MAX;
 string s;
+
+void solve(int start, int end, char b1, char b2, int type)
+{
+    int tmp = 0; bool check = false;
+    if(type){
+        for(int i=start ; i<=end ; i++){
+            if(s[i] == b1) check = true;
+            if(check) if(s[i] == b2) tmp++;
+        } ans = min(ans, tmp);
+    } else{
+        for(int i=start ; i>=end ; i--){
+            if(s[i] == b1) check = true;
+            if(check) if(s[i] == b2) tmp++;
+        } ans = min(ans, tmp);
+    }
+}
 
 int main()
 {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
@@ -26,29 +41,10 @@ int main()
     }
     if(!R || !B) {cout << 0; return 0;}
     else{
-        tmp = 0; check = false;
-        for(int i=0 ; i<s.size() ; i++){
-            if(s[i] == 'R') check = true;
-            if(check) if(s[i] == 'B') tmp++;
-        } ans = min(ans, tmp);
-        
-        tmp = 0; check = false;
-        for(int i=0 ; i<s.size() ; i++){
-            if(s[i] == 'B') check = true;
-            if(check) if(s[i] == 'R') tmp++;
-        } ans = min(ans, tmp);
-
-        tmp = 0; check = false;
-        for(int i=s.size()-1 ; i>=0 ; i--){
-            if(s[i] == 'R') check = true;
-            if(check) if(s[i] == 'B') tmp++;
-        } ans = min(ans, tmp);
-
-        tmp = 0; check = false;
-        for(int i=s.size()-1 ; i>=0 ; i--){
-            if(s[i] == 'B') check = true;
-            if(check) if(s[i] == 'R') tmp++;
-        } ans = min(ans, tmp);
+        solve(0,n-1,'R','B',1);
+        solve(0,n-1,'B','R',1);
+        solve(n-1,0,'R','B',0);
+        solve(n-1,0,'B','R',0);
     }
     cout << ans;
     return 0;
