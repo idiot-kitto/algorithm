5a112f5ac20ed14da81f82b1213bc4afe5e00734
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-10-14 16:29:50 +0900

    #구현

--- programmers/월간 코드 챌린지 시즌1 (10월)/1. 3진법 뒤집기.cpp
+++ programmers/월간 코드 챌린지 시즌1 (10월)/1. 3진법 뒤집기.cpp
@@ -6,7 +6,7 @@ int solution(int n) {
     while(n){
         s += to_string(n%3); 
         n/=3;
-    }//cout << s;
+    }
     int a = 1;
     for(int i=s.size()-1 ; i>=0 ; i--){
         answer += int(s[i]-'0') * a;
