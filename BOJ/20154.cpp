cb81d38847a112712ab74cce6186d13dabb25fba
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-11-16 19:27:09 +0900

    #이 구역의 승자는 누구야?! #C++ #수학 #Bronze1

---
+++ BOJ/20154.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define SIZE 100001
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+/* seg_tree
+ll tree[4*SIZE];
+ll init(ll node, ll start, ll end){
+    ll mid = (start + end) / 2;
+    if(start == end) return tree[node] = v[start];
+    else return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
+void update(ll node, ll start, ll end, ll index, ll diff) {
+	if (index < start || index > end) return;
+	tree[node] = tree[node] + diff;
+	if (start != end) {
+		update(node * 2, start, (start + end) / 2, index, diff);
+		update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+	}
+}
+long long sum(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+    return sum(node * 2, start, (start + end) / 2, left, right) + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+}*/
+
+int main(){fastio
+    string s; cin >> s;
+    int arr[26] = {3,2,1,2,3,3,3,3,1,1,3,1,3,3,1,2,2,2,1,2,1,1,2,2,2,1};
+    deque<int> dq;
+    for(int i=0 ; i<s.size() ; i++) dq.pb(arr[s[i]-'A']);
+    while(dq.size() != 1){
+        deque<int> tmp;
+        for(int i=0 ; i<dq.size() ; i+=2){
+            if(i == dq.size()-1) break;
+            int k = dq[i] + dq[i+1];
+            if(k >= 10) k %= 10;
+            tmp.pb(k);
+        }
+        int c = dq.size();
+        for(int i=0 ; i<c/2 ; i++) {dq.pop_front();dq.pop_front();}
+        while(!tmp.empty()){
+            dq.push_front(tmp.front());
+            tmp.pop_front();
+        }
+    }
+    if(dq[0] % 2) cout << "I'm a winner!";
+    else cout << "You're the winner?";
+    return 0;
+}
