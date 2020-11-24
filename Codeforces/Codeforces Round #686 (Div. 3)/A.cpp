2d7d2c502d5caa253944110070da7599ebd8fd71
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-11-25 01:52:41 +0900

    A - Special Permutation

---
+++ Codeforces/Codeforces Round #686 (Div. 3)/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define SIZE 100001
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+ 
+/* seg_tree
+ll tree[4*SIZE];
+ll init(ll node, ll start, ll end){
+    ll mid = (start + end) / 2;
+    if(start == end) return tree[node] = v[start];
+    else return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
+void update(ll node, ll start, ll end, ll index, ll diff) {
+	if (index < start || index > end) return;
+	tree[node] = tree[node] + diff;
+	if (start != end) {
+		update(node * 2, start, (start + end) / 2, index, diff);
+		update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+	}
+}
+long long sum(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+    return sum(node * 2, start, (start + end) / 2, left, right) + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+}*/
+int t;
+int main(){fastio
+    cin >> t;
+    while(t--){
+        int n; cin >> n;
+        if(n%2){
+            for(int i=n ; i>=1 ; i--){
+                if(i == (n+1)/2) continue;
+                cout << i << ' ';
+            }cout << (n+1)/2 << ' ';
+        }else{
+            for(int i=n ; i>=1 ; i--) cout << i << ' ';
+        }cout << '\n';
+    }
+    return 0;
+}
