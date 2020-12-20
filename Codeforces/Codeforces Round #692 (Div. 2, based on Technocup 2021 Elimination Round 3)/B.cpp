88aefaea538b9cc59e273c4170f974f46f7aa178
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-12-21 03:13:02 +0900

    B - Fair Numbers

---
+++ Codeforces/Codeforces Round #692 (Div. 2, based on Technocup 2021 Elimination Round 3)/B.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+ 
+int main(){fastio
+    int t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        while(1){
+            ll tmp = n; vl v;
+            while(tmp){
+                if(tmp % 10) v.pb(tmp % 10);
+                tmp /= 10;
+            }
+            bool ok = true;
+            while(!v.empty()){
+                ll k = v.back(); v.pop_back();
+                if(n % k){n++; ok = false; break;}
+            } if(ok) {cout << n << '\n'; break;}
+        }
+    }
+    return 0;
+}
