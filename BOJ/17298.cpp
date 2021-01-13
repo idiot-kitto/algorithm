978e9fa92cacfeaf8edf5d0b308c6fa48bc4cfe3
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-01-13 14:32:31 +0900

    #오큰수 #C++ #스택 #Gold4

---
+++ BOJ/17298.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define SIZE 100001
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<pii> vpii;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    int n; cin >> n; vpii v(n); vi ans(n,-1);
+    for(int i=0 ; i<n ; i++) {cin >> v[i].first; v[i].second = i;}
+    deque<pii> dq;
+    for(int i=0 ; i<n ; i++){
+        if(dq.empty() || dq.back().first >= v[i].first) dq.pb({v[i].first, v[i].second});
+        else{
+            while(1){
+                if(dq.empty() || dq.back().first >= v[i].first) break;
+                ans[dq.back().second] = v[i].first;
+                dq.pop_back();
+            }dq.pb({v[i].first, v[i].second});
+        }
+    }
+    for(int i=0 ; i<n ; i++) cout << ans[i] << ' ';
+    return 0;
+}
