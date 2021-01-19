156aad928d11fffd4e74ea798d8fa398b4e971ee
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-01-20 03:29:35 +0900

    #Ezreal 여눈부터 가네 ㅈㅈ #C++ #dp #Gold5

---
+++ BOJ/20500.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    int n; cin >> n; int dp[1520];
+    dp[1] = 0; dp[2] = 1; dp[3] = 1;
+    for(int i=4 ; i<=1515 ; i++){
+        if(i%2) dp[i] = ((dp[i-1] * 2) % mod - 1) % mod;
+        else dp[i] = ((dp[i-1] * 2) % mod + 1) % mod;
+    }cout << dp[n];
+    return 0;
+}
