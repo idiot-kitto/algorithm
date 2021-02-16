d6153eda795c58286c1a57c28bd16dec14108a42
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-02-17 03:06:49 +0900

    A - Dense Array

---
+++ Codeforces/Codeforces Round #702 (Div. 3)/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+ 
+int main(){fastio
+    int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n; vi v(n+1);
+        for(int i=1 ; i<=n ; i++) cin >> v[i];
+        int ans = 0;
+        for(int i=1 ; i<n ; i++){
+            if(2 * min(v[i],v[i+1]) >= max(v[i],v[i+1])) continue;
+            else{
+                int a = min(v[i], v[i+1]);
+                int b = max(v[i], v[i+1]);
+                while(1){
+                    if(a*2 >= b) break;
+                    ans++;
+                    a *= 2;
+                }
+            }
+        }cout << ans << '\n';
+    }
+ 
+    return 0;
+}
