e44f70c2d74c067da9d656689017f7ed8fdd870b
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-03-26 01:46:23 +0900

    A - Strange Table

---
+++ Codeforces/Codeforces Round #710 (Div. 3)/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const int mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    int t; cin >> t;
+    while(t--){
+        ll n, m, x; cin >> n >> m >> x;
+        cout << (x - 1) / n + 1 + ((x - 1) % n) * m << '\n';
+    }
+
+    return 0;
+}
