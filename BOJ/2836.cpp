315861c2a8be53d8c282b2b7789776c72ac34017
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-01-22 19:32:05 +0900

    수상 택시 / 스위핑 / Gold3

---
+++ BOJ/2836.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    ll n, m; cin >> n >> m; vpll v;
+    for(int i=0 ; i<n ; ++i) {
+        ll a, b; cin >> a >> b;
+        if(a > b) v.pb({b, a});
+    } sort(all(v));
+    
+    ll l = v[0].first, r = v[0].second, d = 0;
+    for(auto k : v) {
+        if(r < k.first) d += (r - l) * 2, l = k.first, r = k.second;
+        else if(r < k.second) r = k.second;
+    } cout << d + (r - l) * 2 + m;
+    return 0;
+}
