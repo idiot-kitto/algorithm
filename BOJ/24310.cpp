8e4d170c68e7f8a204c0d1fea05e210cf6337e8e
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-01-24 16:01:52 +0900

    БОЯДИСВАНЕ НА ОГРАДА / 수학 / Bronze3

---
+++ BOJ/24310.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    if(a > b) swap(a, b);
+    if(c > d) swap(c, d);
+    if((c >= a && c <= b) || a >= c && a <= d) { cout << max(b, d) - min(a, c) + 1; return 0; }
+    cout << b - a + 1 + d - c + 1; return 0;
+}
