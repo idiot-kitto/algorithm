28b7a8fd6cfec8e75db22f92922f749473a0ebfc
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-02-06 16:33:59 +0900

    미로에 갇힌 상근 / dp..지만 재미로 전처리 숏코딩 / Gold5

---
+++ BOJ/5069.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){int d[15]={0,0,6,12,90,360,2040,10080,54810,290640,1588356,8676360,47977776,266378112,1488801600};int t;cin>>t;while(t--){int n;cin>>n;cout<<d[n]<<'\n';}return 0;}
