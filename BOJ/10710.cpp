6cd7cc5b8b23856287c1b7dde591eaa197ccc762
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-02-10 16:32:15 +0900

    실크로드 / dp / Gold5

---
+++ BOJ/10710.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int d[1010], c[1010], dp[1010][1010];
+
+int main(){fastio
+    int n, m; cin >> n >> m;
+    for(int i=1 ; i<=n ; ++i) cin >> d[i];
+    for(int i=1 ; i<=m ; ++i) cin >> c[i];
+    
+    for(int i=1 ; i<=n ; ++i) {
+        for(int j=i ; j<=m ; ++j) {
+            dp[i][j] = dp[i-1][j-1] + d[i] * c[j];
+            if(dp[i][j] > dp[i][j-1] && dp[i][j-1]) dp[i][j] = dp[i][j-1];
+        }
+    } cout << dp[n][m];
+
+    return 0;
+}
