9471736e86394b996f35734486523c134b739852
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-02-22 15:56:26 +0900

    blobaww / dp / Gold4

---
+++ BOJ/24501.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+char arr[3030][3030];
+ll dpe[3030][3030], dpm[3030][3030], ans;
+
+int main(){fastio
+    int n, m; cin >> n >> m;
+    for(int i=0 ; i<n ; ++i) {
+        string s; cin >> s;
+        for(int j=0 ; j<m ; ++j) arr[i][j] = s[j];
+    }
+
+    for(int i=0 ; i<n ; ++i) 
+        for(int j=0 ; j<m ; ++j) {
+            dpe[i+1][j+1] = dpe[i+1][j] + dpe[i][j+1] - dpe[i][j];
+            if(arr[i][j] == 'E') dpe[i+1][j+1]++;
+        }
+
+    for(int i=n-1 ; i>=0 ; --i) 
+        for(int j=m-1 ; j>=0 ; --j) {
+            dpm[i][j] = dpm[i+1][j] + dpm[i][j+1] - dpm[i+1][j+1];
+            if(arr[i][j] == 'M') dpm[i][j]++;
+        }
+
+    for(int i=0 ; i<n ; ++i) for(int j=0 ; j<m ; ++j)
+        if(arr[i][j] == 'S') ans += ((dpe[i+1][j+1] * dpm[i][j]) % mod);
+    cout << ans % mod;
+
+    return 0;
+}
