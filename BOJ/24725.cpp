9fc1e22ac3c97924799e25633b014c1b9f72b2fd
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-03-21 16:04:40 +0900

    엠비티아이 / 구현 / Silver3

---
+++ BOJ/24725.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[8][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+char arr[202][202];
+int ans;
+
+int main(){fastio
+    int n, m; cin >> n >> m;
+    for(int i=1 ; i<=n ; ++i) {
+        string s; cin >> s;
+        for(int j=1 ; j<=m ; ++j) arr[i][j] = s[j-1];
+    }
+
+    auto go = [&](int x, int y) {
+        for(int i=0 ; i<8 ; ++i) {
+            bool ok = true;
+            for(int j=1 ; j<=3 ; ++j) {
+                int nx = x + dir[i][0] * j, ny = y + dir[i][1] * j;
+                if(nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
+                    if(j == 1) { if(arr[nx][ny] != 'N' && arr[nx][ny] != 'S') ok = false; }
+                    else if(j == 2) { if(arr[nx][ny] != 'F' && arr[nx][ny] != 'T') ok = false; }
+                    else if(j == 3) { if(arr[nx][ny] != 'P' && arr[nx][ny] != 'J') ok = false; }
+                } else ok = false;
+            } if(ok) ans++;
+        }
+    };
+
+    for(int i=1 ; i<=n ; ++i) for(int j=1 ; j<=m ; ++j) if(arr[i][j] == 'E' || arr[i][j] == 'I') go(i, j);
+    cout << ans;
+
+    return 0;
+}
