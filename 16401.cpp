dffd2aa10334232b1a12afcec913e52ca9d9659d
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-03-22 16:42:46 +0900

    과자 나눠주기 / 이분탐색 / Silver3

---
+++ 16401.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+int main(){fastio
+    int m, n; cin >> m >> n; vi v(n+1);
+    int l = 1, r = 1, ans = 0;
+    for(int i=1 ; i<=n ; ++i) { cin >> v[i]; r = max(r, v[i]); }
+    auto f = [&](int k) {
+        int cnt = 0;
+        for(int i=1 ; i<=n ; ++i) cnt += v[i] / k;
+        if(cnt >= m) return true;
+        else return false;
+    };
+    while(l <= r) {
+        int m = (l + r) / 2;
+        if(f(m)) l = m + 1, ans = max(ans, m);
+        else r = m - 1;
+    } cout << ans;
+
+    return 0;
+}
