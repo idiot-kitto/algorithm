14711d2e6031e9ec5885f9b6430c21fcc7de78ef
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-04-17 19:21:59 +0900

    피보나치 수 2 / dp / Bronze1

---
+++ BOJ/2748.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+ll dp[101];
+
+int main(){fastio
+    dp[1] = 1;
+    for(ll i=2 ; i<=90 ; ++i) dp[i] = dp[i-2] + dp[i-1];
+
+    ll n; cin >> n;
+    cout << dp[n];
+
+    return 0;
+}
