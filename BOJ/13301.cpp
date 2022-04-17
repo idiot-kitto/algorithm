2207ee769ebedd3ffa0af38d23c03c0c8756e7dd
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-04-17 19:38:03 +0900

    타일 장식물 / dp / Silver5

---
+++ BOJ/13301.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+const ll mod = 1e9 + 7;
+const long double PI = 3.141592653589793238462643383279502884197;
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a*b/gcd(a,b);}
+int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+ll dp[88];
+
+int main(){fastio
+    int n; cin >> n;
+    dp[1] = 4;
+    dp[2] = 6;
+    for(int i=3 ; i<=80 ; ++i) dp[i] = dp[i-2] + dp[i-1];
+    cout << dp[n];
+    return 0;
+}
